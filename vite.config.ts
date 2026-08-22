3fcec1fd8d3670b141cdaa749b873caf6628a076
josh90xd <joshuamolina990@gmail.com>
2026-08-22 14:41:10 -0500

    Configure custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES === "true" ? "/clinica-salud-oral/" : "/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: [".ngrok-free.dev"],
